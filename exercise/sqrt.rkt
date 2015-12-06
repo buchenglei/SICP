a942f102d779434b88a7c49ded48a96926e0bc12
buchenglei <mybc724@163.com>
2015-12-06 10:38:51 +0800

    写了一个更好的求平方根的例子

--- exercise/sqrt.rkt
+++ exercise/sqrt.rkt
@@ -1,29 +1,37 @@
 #lang racket
-;;定义精度要求
-(define accuracy 0.001)
-
-(define (sqert-iter guess x)
-  (if (good-enough? guess x)
+(define (sqrt x)
+  ;;定义精度要求
+  (define accuracy 0.001)
+  ;; 定义迭代过程
+  (define (sqert-iter guess x)
+    (if (good-enough? guess x)
       ;; 如果猜测的结果满足精度要求了就返回
       guess
       ;; 如果不是就进入递归,使用更好的猜测
       (sqert-iter (imporve guess x)
                   x)))
+  ;; 获得更好的猜测
+  (define (imporve guess x )
+    (average guess (/ x guess)))
 
-(define (imporve guess x )
-  (average guess (/ x guess)))
+  ;; 求平均值
+  (define (average x y)
+    (/ (+ x y) 2))
 
-(define (average x y)
-  (/ (+ x y) 2))
+  ;; 求平方
+  (define (square x)
+    (* x x))
 
-(define (square x)
-  (* x x))
+  ;; 判断当前的猜测是否返祖当前精度要求
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) accuracy))
 
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) accuracy))
-
-(define (sqrt x)
-  (sqert-iter 1.0 x))
+  ;; 迭代开始的地方
+  ;; 这里是调用过程的地方，
+  ;; 在过程内部定义过程
+  ;; 这个调用一定要放在最后的位置
+  (sqert-iter 1.0 x)
+)
 
 ;;test
 (sqrt 2)
