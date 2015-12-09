9b4efecabba131186173d9d20c8bab2d0f7f885a
buchenglei <mybc724@163.com>
2015-12-09 18:08:19 +0800

    添加了一个快速求乘幂的过程fast-expt

--- exercise/expt.rkt
+++ exercise/expt.rkt
@@ -14,6 +14,17 @@
         (expt-iter b (- counter 1) (* b product))))
   (expt-iter b n 1))
 
+;; 一个进化版本，但还是线性递归的运算过程
+(define (fast-expt b n )
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+(define (square n)
+  (* n n))
+(define (even? n)
+  (= (remainder n 2) 0))
+
 ;; test
 (expt-1 2 10)
 (expt-2 2 10)
+(fast-expt 2 10)
