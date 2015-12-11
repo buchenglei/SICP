8c9492467e758dda313bd91ca203acb3e968f3e0
buchenglei <mybc724@163.com>
2015-12-11 22:30:27 +0800

    实现费马检查

--- exercise/prime.rkt
+++ exercise/prime.rkt
@@ -17,5 +17,33 @@
 
   (= n (smallest-divisor n)))
 
+;; 费马检查
+(define (fast-prime? n times)
+  ;; 计算乘幂的过程
+  (define (expmod base exp m)
+    (cond ((= exp 0) 1)
+          ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
+          (else (remainder (* base (expmod base (- exp 1) m)) m))))
+
+  ;; 尝试下一个数
+  (define (fermat-test n)
+    (define (try-it a)
+      (= (expmod a n n) a))
+    (try-it (+ 1 (random (- n 1)))))
+  
+  ;; 判断是否为偶数
+  (define (even? n)
+    (if (= (remainder n 2) 0)
+        true
+        false))
+  
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+
 ;; test
 (prime? 11)
+(prime? 10)
+(fast-prime? 11 10)
+(fast-prime? 10 10)
