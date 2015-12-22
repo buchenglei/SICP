4bbc358c2f8d9d1b626d84c0c95e7b2f8bcd69ed
buchenglei <mybc724@163.com>
2015-12-22 13:04:28 +0800

    牛顿法

--- exercise/fixed-point.rkt
+++ exercise/fixed-point.rkt
@@ -17,6 +17,7 @@
           (try next))))
   (try first-guess))
 
+(define (square x) (* x x))
 ;; test
 (fixed-point cos 1.0)
 ;; y=siny+cosy
@@ -29,4 +30,26 @@
   (fixed-point (lambda (y) (average y (/ x y)))
                1.0))
 (sqrt 4)
+
+;; 过程作为返回值
+;; 牛顿法
+;; 该过程描述导数的概念
+(define dx 0.00001)
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+;; 求函数 x |-> x^3在5的导数的近似值
+(define (cube x) (* x x x))
+((deriv cube) 5)
+;; 有了deriv之后，牛顿法就可以表述为一个求不动点的过程
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+(define (sqrt2 x)
+  (newtons-method (lambda (y) (- (square y) x))
+                  1.0))
+(sqrt2 4)
 
